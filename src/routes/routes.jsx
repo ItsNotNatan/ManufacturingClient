a5a827046047e650113e23c870bfac19d74eb2b4
Natan Guimarães dos Santos <natanguimaraes158@gmail.com>
2026-09-14 13:57:52 -0300

    Add files via upload

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,17 +4,19 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 // Importação dos nossos componentes estruturais e páginas
 import Layout from '../components/Layout/Layout';
 import Formulario from '../pages/Formulario/Formulario';
+// 1. Importamos a nossa nova tela de Acompanhamento
+import Acompanhamento from '../pages/Acompanhamento/Acompanhamento';
 
 // Criação do mapa de navegação do site
 export const rotas = createBrowserRouter([
     {
-        // 1. Rota Principal (Mãe)
+        // Rota Principal (Mãe)
         // Sempre que o utilizador aceder a qualquer caminho a partir da raiz ('/'),
         // o React vai carregar primeiro o Layout (que tem a Navbar).
         path: '/',
         element: <Layout />,
 
-        // 2. Rotas Filhas (Children)
+        // Rotas Filhas (Children)
         // Estas páginas vão aparecer dentro do <Outlet /> do Layout.
         children: [
             {
@@ -29,14 +31,15 @@ export const rotas = createBrowserRouter([
                 element: <Formulario />
             },
             {
-                // Rota preparada para o teu futuro Dashboard (podes criar depois)
-                path: 'dashboard',
-                element: <div><h1>Página de Dashboard em construção...</h1></div>
+                // 2. Rota atualizada para a nossa nova tela!
+                // Quando o URL for /acompanhamento, mostra a tabela de transportes
+                path: 'acompanhamento',
+                element: <Acompanhamento />
             }
         ]
     },
     {
-        // Exemplo de uma rota FORA do Layout (não terá a Navbar).
+        // Exemplo de uma rota FORA do Layout (não terá a Navbar no topo).
         // Ideal para a futura página de Login.
         path: '/login',
         element: <div><h1>Página de Login Isolada</h1></div>
